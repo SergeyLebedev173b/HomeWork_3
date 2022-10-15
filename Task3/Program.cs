eaa7a6c1e5dbeae05dc26f51c6ea73e7dcb3841f
sergeylebedev <lebedevs06@gmail.com>
2022-10-15 21:32:30 +0400

    Определение чётное-нечётное

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int namber = Convert.ToInt32(Console.ReadLine());
+if( namber % 2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число не чётное");
+}
